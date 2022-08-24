ced48cf0e25b0f8715a95d35817545fef6040e8d
20220876 <110364597+Taeyeob-Kim@users.noreply.github.com>
2022-08-25 10:51:30 +1200

    Update Dog age.py

--- Dog age.py
+++ Dog age.py
@@ -9,3 +9,7 @@ if age<3:
 elif age>2:
     print("dog age is",2*10.5+(age-2)*4)
 
+    ##it was the task week3?
+    ##I made a code to caculate dog age
+    ##However, I believe there is better code for this.
+    ##Is there any better code suggestion? 
