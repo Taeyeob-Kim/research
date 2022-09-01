dd1d919bc3f82700142de0a9238d64ebed9e8c5b
20220876 <110364597+Taeyeob-Kim@users.noreply.github.com>
2022-09-01 17:02:40 +1200

    Create If code.py

---
+++ Python/If code.py
@@ -0,0 +1,10 @@
+a=10
+b=20
+if a > b:
+    print('a is higher than b')
+elif a == b:
+    print('a is equal to b')
+elif a < b:
+    print('b is higher than a')
+    
+   ## If code would be useful to make program has many variable options
