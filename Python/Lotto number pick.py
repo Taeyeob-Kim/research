8ccdbbb45c229b835a0c57507f073df49ec62871
20220876 <110364597+Taeyeob-Kim@users.noreply.github.com>
2022-09-01 17:02:00 +1200

    Create Lotto number pick.py

---
+++ Python/Lotto number pick.py
@@ -0,0 +1,11 @@
+
+import random
+lotto=[]
+while len(lotto)<6:
+    num=random.randint(1,40)
+    if num not in lotto:
+        lotto.append(num)
+    
+    print("the lotto numbers are", lotto)
+    
+     ## I bought a lotto last night and It passed my mind, I may could make code to pick numbers. And this is what I made. 
